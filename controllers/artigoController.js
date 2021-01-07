d88b94231681b2f123ef2c8fc65f2ed6627cc948
iguinn13 <igoor182@hotmail.com>
2021-01-07 15:23:30 -0300

    finalizada as funcionalidades principais de artigos

--- controllers/artigoController.js
+++ controllers/artigoController.js
@@ -7,7 +7,7 @@ const Artigo = require('../models/Artigo');
 
 router.get('/', (req, res) => {
     Artigo.findAll({
-        include: [{model: Categoria, required: true}]
+        include: [{ model: Categoria, required: true }]
     }).then(artigos => {
         res.render('admin/artigo/index', {
             artigos: artigos
@@ -39,7 +39,59 @@ router.post('/cadastrarArtigo', (req, res) => {
 });
 
 router.get('/editar/:id', (req, res) => {
-    
+    let id = req.params.id;
+
+    Artigo.findByPk(id).then(artigo => {
+        if(artigo != null){
+            Categoria.findAll().then(categorias => {
+                res.render('admin/artigo/editar', {
+                    categorias,
+                    artigo
+                });
+            })
+        } else {
+            res.redirect('/admin/artigos');
+        }
+    }).catch(() => {
+        res.redirect('/admin/artigos');
+    });
+});
+
+router.post('/confirmarEdicao', (req, res) => {
+    let id = req.body.id;
+    let titulo = req.body.titulo;
+    let categoria = req.body.categoria;
+    let corpo = req.body.corpo;
+
+    Artigo.update({
+        titulo,
+        categoriaId: categoria,
+        corpo
+    },
+    {
+        where:{
+            id
+        }
+    }
+    ).then(() => {
+        res.redirect('/admin/artigos');
+    }).catch(() => {
+        res.redirect('/admin/artigos');
+    });
+});
+
+router.post('/deletar', (req, res) => {
+    let id = req.body.id;
+
+    Artigo.destroy({
+        where:{
+            id
+        }
+    }).then(() => {
+        res.redirect('/admin/artigos');
+    }).catch(() => {
+        res.redirect('/admin/artigos');
+    });
 });
 
 module.exports = router;
